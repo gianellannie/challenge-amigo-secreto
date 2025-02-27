2b3fde603290dd9a4990a58d5b31a7a850b167d5
Gianella Annie <gianella.annie.060@gmail.com>
2025-02-27 12:26:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  const inputAmigo = document.getElementById('amigo');
+  const valueAmigo = inputAmigo.value.trim();
+  if (valueAmigo === '') {
+    alert('Por favor, inserte un nombre.');
+  } else {
+    amigos.push(valueAmigo);
+    actualizarListaAmigos();
+  }
+  inputAmigo.value = '';
+}
+
+function actualizarListaAmigos() {
+  const ulListaAmigos = document.getElementById('listaAmigos');
+  ulListaAmigos.innerHTML = '';
+  for (let index = 0; index < amigos.length; index++) {
+    ulListaAmigos.innerHTML += `<li>${amigos[index]}</li>`;
+  }
+}
+
+function sortearAmigo() {
+  if (!amigos.length) return;
+  const ulResultado = document.getElementById('resultado');
+  const indexAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigo = amigos[indexAleatorio];
+  ulResultado.innerHTML = `<li>${amigo}</li>`;
+}
